f329240a401176c4d3c89424046b2d797f1d7a58
adrienhongcs <adrien.hong.cs@gmail.com>
2021-05-18 14:16:01 -0400

    Initial Commit

---
+++ GET A QUOTE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GET A QUOTE
+//
+//  Created by Adrien Hong on 5/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GET A QUOTE/GET_A_QUOTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GET_A_QUOTEApp.swift
+//  GET A QUOTE
+//
+//  Created by Adrien Hong on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct GET_A_QUOTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
